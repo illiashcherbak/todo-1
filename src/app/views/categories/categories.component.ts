9fbe08775dea1874437fb35e5cb82a3f84830ef1
Illia Shcherbak <illiashcherbak6@gmail.com>
2023-05-30 14:38:06 +0300

    added the display of categories in the left menu

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-  constructor() {
+
+  categories: Category[] = [];
+
+  constructor(private dataHandler: DataHandlerService) {
   }
 
+  // The method is called automatically after initialization of the component
   ngOnInit() {
-
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories)
   }
 }
